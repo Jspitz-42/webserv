1e331d94a40cd74d5dab2defc38503e96ef2fa85
Joshua Spitz <jspitz@f6r13s8.clusters.42paris.fr>
2025-08-17 08:37:08 +0200

    --wip-- [skip ci]

--- html_file/cgi/test.py
+++ html_file/cgi/test.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3 #chatGPT
-import cgi
 import os
-import sys
+import cgitb
+
+cgitb.enable(display=0, logdir="/logDir")
 
 print("Content-Type: text/html")
 print()
 
 print("<html><body>")
 print("<h1>Test CGI Python</h1>")
+print("Bonjour le monde")
 
 method = os.environ.get("REQUEST_METHOD", "")
 print(f"<p>Méthode HTTP : {method}</p>")
